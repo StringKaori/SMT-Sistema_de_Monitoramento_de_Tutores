7fc56488a7ed45b71fc78affe8a9c6d91c9c3e3d
Guilherme Leite <guilherme99leite@gmail.com>
2025-06-11 15:39:06 -0300

    fix: passing navigation to the `ProfessorCard` at the `FavoritesScreen`.

--- app/modules/Favorites/FavoritesScreen.tsx
+++ app/modules/Favorites/FavoritesScreen.tsx
@@ -2,9 +2,12 @@ import { ProfessorCard } from "@common/components";
 import { View, StyleSheet, FlatList } from "react-native";
 import mock from './mock/mock.json';
 import { ProfessorData } from "@common/types/ProfessorCardData";
+import { useNavigation } from "@react-navigation/native";
+import { RootStackNavigationProp } from "@common/types/RootStackNavigationProp";
 
 const FavoritesScreen = () => {
   const mockFavoriteProfessors: ProfessorData[] = mock 
+  const navigation = useNavigation<RootStackNavigationProp>();
   return (
     <View style={styles.container}>
       <FlatList
@@ -13,7 +16,7 @@ const FavoritesScreen = () => {
         key={"GridView"}
         style={{ paddingTop: 20 }}
         showsVerticalScrollIndicator={false}
-        renderItem={({ item }) => <ProfessorCard data={item} />}
+        renderItem={({ item }) => <ProfessorCard data={item} navigation={navigation} />}
       />
     </View>
   );
